dffc9be5c865d99d494d7ad3d0f25b34ea3e0c94
naymin <naymin@inspiron5577.lan>
2020-10-25 23:40:45 +0630

    mobile menu

--- components/core/blocks/HeaderMenu/HomeMenu.vue
+++ components/core/blocks/HeaderMenu/HomeMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="cat" class="cat-dropdown" v-if="show">
+  <!-- <div id="cat" class="cat-dropdown" v-if="show">
     <div class="sidebar-contant">
       <div id="menu" class="navbar-collapse collapse">
         <div class="top-right-link mobile right-side">
@@ -57,89 +57,6 @@
         </ul>
       </div>
     </div>
-    <!-- <li class="level">
-            <a href="shop.html" class="page-scroll"><i class="fa fa-camera-retro" />Cameras</a>
-          </li>
-          <li class="level sub-megamenu ">
-            <span class="opener plus" />
-            <a href="shop.html" class="page-scroll"><i class="fa fa-tablet" />Smartphones</a>
-            <div class="megamenu mobile-sub-menu" style="width:630px;">
-              <div class="megamenu-inner-top">
-                <ul class="sub-menu-level1">
-                  <li class="level2">
-                    <a href="shop.html"><span>Women Clothings</span></a>
-                    <ul class="sub-menu-level2">
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Dresses</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Sport Jeans</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Skirts</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Tops</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Sleepwear</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Jeans</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="level2">
-                    <a href="shop.html"><span>Men Clothings</span></a>
-                    <ul class="sub-menu-level2 ">
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Blazer & Coat</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Sport Shoes</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Phone Cases</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Trousers</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Purse</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Wallets</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="level2">
-                    <a href="shop.html"><span>Juniors kid</span></a>
-                    <ul class="sub-menu-level2 ">
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Blazer & Coat</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Sport Shoes</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Phone Cases</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Trousers</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Purse</a>
-                      </li>
-                      <li class="level3">
-                        <a href="shop.html"><span>■</span>Wallets</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li> -->
-    </ul>
     <div class="header-top mobile">
       <div class="">
         <div class="row">
@@ -173,9 +90,98 @@
         </div>
       </div>
     </div>
-  </div>
-  </div>
-  </div>
+  </div> -->
+  <div id="cat" class="cat-dropdown">
+    <div class="sidebar-contant">
+      <div id="menu" class="navbar-collapse collapse menu-open">
+        <div class="top-right-link mobile right-side">
+          <ul>
+            <li class="login-icon content">
+              <a class="content-link">
+                <span class="content-icon" />
+              </a>
+              <a href="login.html" title="Login">Login</a> or
+              <a href="register.html" title="Register">Register</a>
+              <div class="content-dropdown">
+                <ul>
+                  <li class="login-icon">
+                    <a href="login.html" title="Login"><i class="fa fa-user" /> Login</a>
+                  </li>
+                  <li class="register-icon">
+                    <a href="register.html" title="Register"><i class="fa fa-user-plus" /> Register</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="track-icon">
+              <a title="Track your order"><span /> Track your order</a>
+            </li>
+            <li class="gift-icon">
+              <a href="" title="Gift card"><span /> Gift card</a>
+            </li>
+          </ul>
+        </div>
+        <ul class="nav navbar-nav ">
+          <li class="level"
+              v-for="category in visibleCategories" :key="category.slug"
+              :class="category.children_count > 0 ? 'sub-megamenu': '' "
+          >
+            <span class="opener plus" v-if="category.children_count > 0" />
+            <router-link
+              :to="'/'+category.url_path"
+              data-testid="category.url_path"
+              class="page-scroll"
+            >
+              <i class="fa fa-female" />{{ category.name }}
+            </router-link>
+            <div class="megamenu mobile-sub-menu " style="215px" v-if="category.children_count > 0 ">
+              <div class="megamenu-inner-top">
+                <sub-category
+                  :category-links="category.children_data"
+                  :id="category.id"
+                  :parent-slug="category.slug"
+                  :parent-path="category.url_path"
+                  v-if="category.children_count > 0"
+                />
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div class="header-top mobile">
+          <div class="">
+            <div class="row">
+              <div class="col-12">
+                <div class="top-link top-link-left select-dropdown ">
+                  <fieldset>
+                    <select name="speed" class="country option-drop" id="ui-id-3" style="display: none;">
+                      <option selected="selected">
+                        English
+                      </option>
+                      <option>Frence</option>
+                      <option>German</option>
+                    </select><span class="ui-selectmenu-button ui-widget ui-state-default ui-corner-all" tabindex="0" id="ui-id-3-button" role="combobox" aria-expanded="false" aria-autocomplete="list" aria-owns="ui-id-3-menu" aria-haspopup="true" style="width: 82px;"><span class="ui-icon ui-icon-triangle-1-s" /><span class="ui-selectmenu-text">English</span></span>
+                    <select name="speed" class="currency option-drop" id="ui-id-4" style="display: none;">
+                      <option selected="selected">
+                        USD
+                      </option>
+                      <option>EURO</option>
+                      <option>POUND</option>
+                    </select><span class="ui-selectmenu-button ui-widget ui-state-default ui-corner-all" tabindex="0" id="ui-id-4-button" role="combobox" aria-expanded="false" aria-autocomplete="list" aria-owns="ui-id-4-menu" aria-haspopup="true" style="width: 82px;"><span class="ui-icon ui-icon-triangle-1-s" /><span class="ui-selectmenu-text">USD</span></span>
+                  </fieldset>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="top-link right-side">
+                  <div class="help-num">
+                    Need Help? : 03 233 455 55
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
