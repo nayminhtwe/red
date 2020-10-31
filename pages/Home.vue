716c930251e0d76769f6d3bcbd9a2dc1d4196ab3
naymin <konayminhtwe@gmail.com>
2020-10-31 13:32:51 +0630

    banner

--- pages/Home.vue
+++ pages/Home.vue
@@ -2,6 +2,16 @@
   <div id="home">
     <head-image />
     <featured-blocks />
+    <section class="pt-70">
+      <div class="container">
+        <h2 align="center">
+          Welcome to 21Marts
+        </h2>
+        <h4 align="center">
+          Lorem khaled ipsum is a major key to success. It’s on you how you want to live your life. Always remember in the jungle there’s a lot of they in.
+        </h4>
+      </div>
+    </section>
     <featured-categories />
     <lazy-hydrate :trigger-hydration="!loading" v-if="isLazyHydrateEnabled">
       <featured-products :products="getEverythingNewCollection" />
