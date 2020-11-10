8b95aaa4e266a7b08dfc78fe9257a2658f952475
naymin <konayminhtwe@gmail.com>
2020-11-10 19:21:58 +0630

    welcome

--- components/core/blocks/Featured/Welcome.vue
+++ components/core/blocks/Featured/Welcome.vue
@@ -2,7 +2,7 @@
   <section class="pt-70">
     <div class="container">
       <h2 align="center">
-        Welcome to 21Marts
+        Welcome to Eaindra
       </h2>
       <h4 align="center">
         Lorem khaled ipsum is a major key to success. It’s on you how you want to live your life. Always remember in the jungle there’s a lot of they in.
