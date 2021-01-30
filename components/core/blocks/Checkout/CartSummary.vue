66595b7f1a365e00946884aaee3b58c0f05710c8
naymin <konayminhtwe@gmail.com>
2021-01-30 11:24:20 +0630

    display for addition 5% according to price rule

--- components/core/blocks/Checkout/CartSummary.vue
+++ components/core/blocks/Checkout/CartSummary.vue
@@ -6,7 +6,7 @@
       </h3>
       <product v-for="product in productsInCart" :key="product.server_item_id || product.id" :product="product" />
       <div v-if="productsInCart && productsInCart.length" class="checkout bg-cl-secondary pt10 serif cl-accent">
-        <div v-for="(segment, index) in totals" :key="index" class="row pt15 pb20 pl30 pr55 " v-if="segment.code !== 'grand_total'">
+        <div v-for="(segment, index) in totals" :key="index" class="row pt15 pb20 pl30 pr55 " v-if="segment.code !== 'discount'">
           <div class="col-xs cl-accent">
             {{ segment.title }}
           </div>
@@ -15,14 +15,14 @@
           </div>
         </div>
 
-        <div class="row pt20 pb20 pl30 pr55 weight-400 h3" v-for="(segment, index) in totals" :key="index" v-if="segment.code === 'grand_total'">
+        <!-- <div class="row pt20 pb20 pl30 pr55 weight-400 h3" v-for="(segment, index) in totals" :key="index" v-if="segment.code === 'grand_total'">
           <div class="col-xs">
             {{ segment.title }}
           </div>
           <div class="col-xs align-right">
             {{ segment.value | price(storeView) }}
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
     <div class="py50 px25">
